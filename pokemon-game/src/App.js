f7b5784707e9323f61bacdd4c2b3afbd950536cd
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-01-27 23:43:14 +0200

    First HomeWark

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/HeaderBlock";
+import Layout from "./components/LayoutBlock";
+import Footer from "./components/FooterBlock";
+//import Reactbg1 from "./components/img/bg1.jpg";
+const App = () => {
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
-  );
+    <>
+      <Header
+        title='This is new Titel'
+        descr='This is new description'
+      />
+      <Layout
+        id="1"
+        title='This is new Titel id1' 
+        descr='This is new description'
+        />
+      <Layout
+        hideBackground
+        title='This is new Titel id2' 
+        descr='This is new description'
+        id="2" />
+      <Layout
+        title='This is new Titel id3' 
+        descr='This is new description'
+        id="3" />
+      <Footer />
+    </>
+  )
 }
+ 
 
 export default App;
---
+++ pokemon-game/src/components/LayoutBlock/index.js
@@ -0,0 +1,29 @@
+import s from'./style.module.css';
+
+
+const Layout = ({ hideBackground = false, title, descr, id }) => {
+  const styleRoot = hideBackground ? { backgroundImage: "none" } : {};
+    return (
+    <>
+        <section
+          style={styleRoot}
+          className={s.root}
+          id={id}
+      >
+          <div  className={s.wrapper}>
+              <article >
+              <div className={s.title}>
+                    <h3>{title}</h3>
+                    <span className={s.separator}></span>
+                  </div>
+                  <div className={s.descFull} >
+                      <p>{descr}</p>
+                  </div>
+              </article>
+          </div>
+      </section>
+    </>
+  )
+}
+
+export default Layout;
